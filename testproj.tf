328069a5564673064c145e014b6280e4d4005dbc
Srini <srinivas.thota5@gmail.com>
2023-12-03 12:43:05 -0500

    Testcases fix

--- testproj.tf
+++ testproj.tf
@@ -1,47 +1,40 @@
 
-// Define the provider
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-// Create the ECS cluster
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "my-cluster"
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = "my-node-lambda"
+  runtime = "nodejs14.x"
+  handler = "index.handler"
+  timeout = 10
+  memory_size = 128
+
+  role = aws_iam_role.lambda_role.arn
+
+  filename = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
 }
 
-// Create the task definition
-resource "aws_ecs_task_definition" "my_task" {
-  family                   = "my-task"
-  container_definitions    = <<DEFINITION
-[
-  {
-    "name": "my-container",
-    "image": "nginx:latest",
-    "portMappings": [
-      {
-        "containerPort": 80,
-        "hostPort": 80,
-        "protocol": "tcp"
-      }
-    ]
-  }
-]
-DEFINITION
+resource "aws_iam_role" "lambda_role" {
+  name = "my-lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
 
-// Create the ECS service
-resource "aws_ecs_service" "my_service" {
-  name            = "my-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
-  desired_count   = 1
-
-  deployment_minimum_healthy_percent = 100
-  deployment_maximum_percent         = 200
-
-  network_configuration {
-    subnets         = ["subnet-12345678"]
-    security_groups = ["sg-12345678"]
-  }
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
